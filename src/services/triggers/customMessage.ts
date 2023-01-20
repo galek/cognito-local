8839d8303dc9ed88b72c4175f64094534c4923a8
Nick Galko <nikolay.galko@gmail.com>
2023-01-20 03:10:01 +0300

    buildFix

--- src/services/triggers/customMessage.ts
+++ src/services/triggers/customMessage.ts
@@ -1,25 +1,17 @@
-import { AttributeListType } from "aws-sdk/clients/cognitoidentityserviceprovider";
-import { CustomMessageTriggerResponse, Lambda } from "../lambda";
-import { attributesToRecord } from "../userPoolService";
-import { Trigger } from "./trigger";
+import {AttributeListType} from "aws-sdk/clients/cognitoidentityserviceprovider";
+import {CustomMessageTriggerResponse, Lambda} from "../lambda";
+import {attributesToRecord} from "../userPoolService";
+import {Trigger} from "./trigger";
 
 const AWS_USERNAME_PARAMETER = "{username}";
 const AWS_CODE_PARAMETER = "{####}";
 
-export type CustomMessageTrigger = Trigger<
-  {
-    source:
-      | "CustomMessage_SignUp"
-      | "CustomMessage_AdminCreateUser"
-      | "CustomMessage_ResendCode"
-      | "CustomMessage_ForgotPassword"
-      | "CustomMessage_UpdateUserAttribute"
-      | "CustomMessage_VerifyUserAttribute"
-      | "CustomMessage_Authentication";
+interface CustomMessageValue {
+    code: string;
+    source: | "CustomMessage_SignUp" | "CustomMessage_AdminCreateUser" | "CustomMessage_ResendCode" | "CustomMessage_ForgotPassword" | "CustomMessage_UpdateUserAttribute" | "CustomMessage_VerifyUserAttribute" | "CustomMessage_Authentication";
     userPoolId: string;
     clientId: string | null;
     username: string;
-    code: string;
     userAttributes: AttributeListType;
 
     /**
@@ -39,51 +31,48 @@ export type CustomMessageTrigger = Trigger<
      * Source: https://docs.aws.amazon.com/cognito/latest/developerguide/user-pool-lambda-custom-message.html#cognito-user-pools-lambda-trigger-syntax-custom-message
      */
     clientMetadata: Record<string, string> | undefined;
-  },
-  CustomMessageTriggerResponse | null
->;
-
-interface CustomMessageServices {
-  lambda: Lambda;
 }
 
-export const CustomMessage =
-  ({ lambda }: CustomMessageServices): CustomMessageTrigger =>
-  async (
-    ctx,
-    {
-      clientId,
-      clientMetadata,
-      code,
-      source,
-      userAttributes,
-      username,
-      userPoolId,
-    }
-  ) => {
-    try {
-      const response = await lambda.invoke(ctx, "CustomMessage", {
-        clientId,
-        clientMetadata,
-        codeParameter: AWS_CODE_PARAMETER,
-        triggerSource: source,
-        userAttributes: attributesToRecord(userAttributes),
-        username,
-        usernameParameter: AWS_USERNAME_PARAMETER,
-        userPoolId,
-      });
+export type CustomMessageTrigger = Trigger<CustomMessageValue, CustomMessageTriggerResponse | null>;
 
-      return {
-        emailMessage: response.emailMessage
-          ?.replace(AWS_CODE_PARAMETER, code)
-          .replace(AWS_USERNAME_PARAMETER, username),
-        emailSubject: response.emailSubject,
-        smsMessage: response.smsMessage
-          ?.replace(AWS_CODE_PARAMETER, code)
-          .replace(AWS_USERNAME_PARAMETER, username),
-      };
+interface CustomMessageServices {
+    lambda: Lambda;
+}
+
+// it's copy of BaseCustomMessageTriggerEvent.response interface from aws-lambda
+export interface BaseCustomMessageResponse {
+    smsMessage: string | null;
+    emailMessage: string | null;
+    emailSubject: string | null
+}
+
+export const CustomMessage = ({lambda}: CustomMessageServices): CustomMessageTrigger => async (ctx, {
+    clientId, clientMetadata, code, source, userAttributes, username, userPoolId,
+}): Promise<BaseCustomMessageResponse | null> => {
+    try {
+        const response = await lambda.invoke(ctx, "CustomMessage", {
+            clientId,
+            clientMetadata,
+            codeParameter: AWS_CODE_PARAMETER,
+            triggerSource: source,
+            userAttributes: attributesToRecord(userAttributes),
+            username,
+            usernameParameter: AWS_USERNAME_PARAMETER,
+            userPoolId,
+        });
+
+        const emailMessage = response.emailMessage ? response.emailMessage.replace(AWS_CODE_PARAMETER, code)
+            .replace(AWS_USERNAME_PARAMETER, username) : null;
+        const smsMessage = response.smsMessage ? response.smsMessage.replace(AWS_CODE_PARAMETER, code)
+            .replace(AWS_USERNAME_PARAMETER, username) : null;
+
+        return {
+            emailMessage,
+            emailSubject: response.emailSubject,
+            smsMessage,
+        };
     } catch (ex) {
-      ctx.logger.error(ex);
-      return null;
+        ctx.logger.error(ex);
+        return null;
     }
-  };
+};
